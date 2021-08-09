74cc95c0ac0e60fa0f465010df6b8ccbf6af9fc9
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-08-09 15:34:13 -0500

    Style webpage

--- src/page-template.js
+++ src/page-template.js
@@ -7,19 +7,17 @@ const generatePage = templateData => { // templateData is an array of employee o
       <h2>${manager.name}</h2>
       <h3>${manager.role}</h3>
     </div>
-      <div class="employee-main">
-          <div class="employee-info">
-              <div class="employee-id">
-                <p>ID: ${manager.id}</p>
-              </div>
-              <div class="employee-email">
-                <p>Email: ${manager.email}</p>
-              </div>
-              <div class="employee-special">
-                <p>Office Number: ${manager.officeNumber}</p>
-              </div>
-          </div>
-      </div>
+        <div class="employee-info">
+            <div class="employee-id">
+              <p>ID: ${manager.id}</p>
+            </div>
+            <div class="employee-email">
+              <p>Email: ${manager.email}</p>
+            </div>
+            <div class="employee-special">
+              <p>Office Number: ${manager.officeNumber}</p>
+            </div>
+        </div>
   </div>`
   };
 
@@ -29,17 +27,15 @@ const generatePage = templateData => { // templateData is an array of employee o
       <h2>${engineer.name}</h2>
       <h3>${engineer.role}</h3>
     </div>
-    <div class="employee-main">
-      <div class="employee-info">
-        <div class="employee-id">
-          <p>ID: ${engineer.id}</p>
-        </div>
-        <div class="employee-email">
-          <p>Email: ${engineer.email}</p>
-        </div>
-        <div class="employee-special">
-          <p>GitHub: ${engineer.github}</p>
-        </div>
+    <div class="employee-info">
+      <div class="employee-id">
+        <p>ID: ${engineer.id}</p>
+      </div>
+      <div class="employee-email">
+        <p>Email: ${engineer.email}</p>
+      </div>
+      <div class="employee-special">
+        <p>GitHub: ${engineer.github}</p>
       </div>
     </div>
   </div>`
@@ -51,17 +47,15 @@ const generatePage = templateData => { // templateData is an array of employee o
       <h2>${intern.name}</h2>
       <h3>${intern.role}</h3>
     </div>
-    <div class="employee-main">
-      <div class="employee-info">
-        <div class="employee-id">
-          <p>ID: ${intern.id}</p>
-        </div>
-        <div class="employee-email">
-          <p>Email: ${intern.email}</p>
-        </div>
-        <div class="employee-special">
-          <p>School: ${intern.school}</p>
-        </div>
+    <div class="employee-info">
+      <div class="employee-id">
+        <p>ID: ${intern.id}</p>
+      </div>
+      <div class="employee-email">
+        <p>Email: ${intern.email}</p>
+      </div>
+      <div class="employee-special">
+        <p>School: ${intern.school}</p>
       </div>
     </div>
   </div>`
